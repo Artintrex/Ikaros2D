7cec30e7c0ffb077be29f735db3285270a3662c1
Egemen Gungor <gamender@gmail.com>
2019-10-24 15:59:38 +0900

    Added project files and some project settings

---
+++ Ikaros2D/IkarosCore.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <windows.h>
+#include <d3d9.h>
+#include <d3dx9.h>
+#include "Box2D.h"
+
+
+//main loop
+
+//prepare a window
+
+//load objects into a list
+
+//update list
+
+//draw list
---
+++ Ikaros2D/IkarosCore.h
@@ -0,0 +1,13 @@
+#pragma once
+
+//Object
+//GameObject:Object
+//Component:Object
+//Behavior:Component
+//MonoBehavior:Behavior
+
+//Sprite:Object
+
+//Renderer:Component
+
+//RigidBody:Component
