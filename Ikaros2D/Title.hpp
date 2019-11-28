93a36bc00aa6081b779c82fc6d0e03699d9d3eb2
hujian <hujian@LAPTOP-FCF9A73L>
2019-11-28 15:15:49 +0900

    簡単なTITLE

--- Ikaros2D/Title.hpp
+++ Ikaros2D/Title.hpp
@@ -8,12 +8,23 @@ public:
 	GameObject* Exit;
 	GameObject* Editor;
 	GameObject* ButtonBackdrop;
-
+	GameObject* MainCamera;
+	int BUTTON = 0, BACKDROPPOS = 0;
+	int BUTTONS[3] = { -790,-755,-830 };
 	void Awake() {
 
 	}
 
 	void Start() {
+
+		MainCamera = new GameObject("MainCamera");
+		Camera* cam = MainCamera->AddComponent<Camera>();
+		MainCamera->transform->Translate(0, 0, -40);
+		cam->orthographic = true;
+		cam->orthographicSize = 1;
+		cam->SetProjection();
+
+
 	TitleBack = new GameObject("TiTleBack");
 	Option = new GameObject("Option");
 	Exit = new GameObject("Exit");
@@ -45,15 +56,51 @@ public:
 	EditorSpr->GenereteSprite();
 	ButtonBackdropSpr->GenereteSprite();
 
-	TitleBack->transform->Scale(0.75,0.75,0.75);
-	Option->transform->Scale(0.5, 0.5, 0.5);
-	Exit->transform->Scale(0.5, 0.5, 0.5);
-	Editor->transform->Scale(0.5, 0.5, 0.5);
-	ButtonBackdrop->transform->Scale(0.5, 0.5, 0.5);
+
+	TitleBack->AddComponent<Renderer>()->sprite = TitleBackSpr;
+	TitleBack->transform->position = Vector3(0, 0, 0.0010);
+	TitleBack->transform->Scale(100, 100,1);
+	
+	ButtonBackdrop->AddComponent<Renderer>()->sprite = ButtonBackdropSpr;
+	ButtonBackdrop->transform->Scale(100, 100, 1);
+	ButtonBackdrop->transform->position = Vector3(-700, -BACKDROPPOS, 0011);
+
+	Option->AddComponent<Renderer>()->sprite = OptionSpr;
+	Option->transform->position = Vector3(BUTTONS[0], 0, 0.0012);
+	Option->transform->Scale(100, 100, 1);
+	
+
+	Editor->AddComponent<Renderer>()->sprite = EditorSpr;
+	Editor->transform->Scale(100, 100, 1);
+	Editor->transform->position = Vector3(BUTTONS[1], -100, 0.0012);
+
+
+	Exit->AddComponent<Renderer>()->sprite = ExitSpr;
+	Exit->transform->Scale(100, 100, 1);
+	Exit->transform->position = Vector3(BUTTONS[2], -200, 0.0012);
+	
+
+
 
 	}
 
 	void Update() {
-
+		
+		if (GetKeyDown(DIK_S))
+		{
+			BUTTON++;
+			if (BUTTON > 2)BUTTON = 0;
+			BACKDROPPOS = BUTTON * 100;
+		}
+		if (GetKeyDown(DIK_W))
+		{
+			BUTTON--;
+			if (BUTTON < 0)BUTTON = 2;
+			BACKDROPPOS = BUTTON * 100;
+		}
+		ButtonBackdrop->transform->position = Vector3(-700, -BACKDROPPOS, 0.0011);
+		
+		
+	
 	}
 };
