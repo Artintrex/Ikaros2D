a3ad956e6fb25eff8e9d41521cd028de2e611f08
game world <mylifeworldgame0000@gmail.com>
2020-01-30 11:46:26 +0900

    Added thunder

--- Ikaros2D/GodsThunder.hpp
+++ Ikaros2D/GodsThunder.hpp
@@ -3,44 +3,70 @@
 
 
 
-class Template : public MonoBehavior {
+class Thunderbolt : public MonoBehavior {
 public:
-	Template() {
+	Thunderbolt() {
 		type = typeid(*this).name();
 		mb_init();
 	}
-	
+
 	std::vector <Sprite*> ThunderSprite;
-	std::vector<Sprite*> Effect;
+	std::vector<Sprite*> ThunderEffect;
 	Renderer* ThunderRenderer;
 	Renderer* EffectRenderer;
 	Camera* mainCamera;
+	Sprite* Thundersprite;
+	Texture* ThundrTexture;
 
 	//—‹‘Ò‹@ŽžŠÔ
 	float ThunderTime = 60;
+	
 	//—‹‚ð~‚ç‚·ŽžŠÔ
 	float GodsThunderTime = 30;
+	
 	RigidBody* RigidThunder;
 	RigidBody* RigidEffect;
 
+	float y;
+
 	void Awake() {
-		GameObject* zeus = GameObject::Find("zeus");
+
 	}
 
 	void Start() {
+		
+		transform->position = mainCamera->ScreenToWorldPoint(Vector3(0, 0, transform->position.z));
+
 		RigidThunder->AddForce(Vector2(0, 10), VelocityChange);
 
-		RigidThunder->AddBoxCollider(Vector2(ThunderSprite[0]->size.x * parent->transform->scale.x * 0.5f,
-			ThunderSprite[0]->size.y * parent->transform->scale.y * 1.5));
+		mainCamera = Camera::main;
+
+		Camera::ScreenToWorldPoint;
+
+		RigidThunder->AddBoxCollider(Vector2(ThunderSprite[0]->size.x * parent->transform->scale.x, 0.1f));
+
+		RigidThunder = parent->AddComponent<RigidBody>();
+
 		ThunderRenderer = parent->AddComponent<Renderer>();
 		EffectRenderer = parent->AddComponent<Renderer>();
-		mainCamera = Camera::main;
-		transform->position.x = mainCamera->transform->position.x + (rand() % 256) - 128;
-		transform->position.y = mainCamera->transform->position.y + 900;
+
+		Thundersprite->texture = ThundrTexture;
+
+		transform->position.x = mainCamera->transform->position.x + (rand() % 350) - 128;
+		transform->position.y = transform->position.y + 300;
+		y = transform->position.y;
 
 	}
 
 	void Update() {
+
+		if (transform->position.y < y - ThunderSprite.size.y)
+		{
+			ThunderSprite[0] = new Sprite("Thunder");
+		}
+
+
+		Thundersprite->GenereteSprite();
 
 
 		ThunderTime -= Time.DeltaTime;
@@ -52,8 +78,10 @@ public:
 void OnCollisionEnter(Collision collider) {
 
 	if (collider.parent->tag == "Ground") {
-		
+
+
 		//effect
+		//ThunderEffect[0] = new Sprite("ThunderEffect");
 
 	}
 
